508d704ef4bcb9a1e9aa036921f5d97c27a82750
alejo9010 <alejandrosanchez9010@gmail.com>
2023-06-22 17:20:05 -0500

    Providers for auth and supabase

---
+++ providers/SupabaseProvider.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { Database } from '@/types_db';
+import { useState } from 'react';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+
+interface SupabaseProviderProps {
+  children: React.ReactNode;
+}
+
+function SupabaseProvider({ children }: SupabaseProviderProps) {
+  const [supabaseClient] = useState(() => {
+    return createClientComponentClient<Database>();
+  });
+
+  return (
+    <SessionContextProvider supabaseClient={supabaseClient}>{children}</SessionContextProvider>
+  );
+}
+
+export default SupabaseProvider;
